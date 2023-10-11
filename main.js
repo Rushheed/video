cf0b24b33283761334cf1bc11d33e85112760ce3
Oladoja Abdulrasheed <oladojaroyalrush@gmail.com>
2023-10-12 00:40:03 +0100

    Change videos

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-const text = document.getElementById('text')
+const text = document.getElementById('text');
 
 function handleSubmit() {
     console.log(text.value);
 
-    let message = text.value 
+    let message = text.value; 
     console.log({mes: message});
     if (message.length == 0) {
         alert('Kindly Fill in a comment')
         return 
     }
     localStorage.setItem('videoComment', JSON.stringify(message));
+
+    text.value = '';
+
+    console.log('Message submitted:', message);
 }
